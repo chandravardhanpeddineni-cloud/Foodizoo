69f92de4e600c17f02f9fc8e77a28a79b824be77
Chandra Vardhan <chandravardhanpeddineni@gmail.com>
2026-08-20 14:40:59 +0530

    solved production cors allowed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(
   cors({
     // origin: "https://genie-food-app.netlify.app",
-    origin: "http://localhost:5173",
+    origin: "https://foodizoo-frontend-kvgvvhcpz.vercel.app",
     credentials: true,
   }),
 );
